71b0335da63eac04bb44d51947e86bceff23c27a
Shoulder <40721597+Axelmannen@users.noreply.github.com>
2021-04-22 14:28:47 +0200

    Games can now be started

--- server/llamaSoltorg/routes/index.js
+++ server/llamaSoltorg/routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 
 
 /** Contains all active games */
-let games = [] // TODO: Save this to file and load it on startup
+let activeGames = []
 
 // Handle POST-request to create game
 router.post('/create-game', function(request, response) {
@@ -24,12 +24,13 @@ router.post('/create-game', function(request, response) {
     "gameState" : 0,  // Game has not started
     "players" : [
       {
-        "username" : username
+        "username" : username,
+        "points" : 0,
       }
     ]
   }
 
-  games.push(gameObject)
+  activeGames.push(gameObject)
 
   response.send(gameObject)
 })
@@ -43,7 +44,7 @@ router.post('/join-game', function(request, response) {
   let gameID = request.body.gameID
 
   // Find game with matching gameID
-  let game = games.find(game => game.gameID == gameID)
+  let game = activeGames.find(game => game.gameID == gameID)
   if (game == undefined) {
     response.send("game not found")
   } else if (game.gameState != 0) {
@@ -55,11 +56,42 @@ router.post('/join-game', function(request, response) {
     // Add new player and return game object to client
     game.players.push({
       "username" : username,
+      "points" : 0,
     })
     response.send(game)
   }
 })
 
+
+// Handle POST-request to start an already created game
+router.post('/start-game', function(request, response) {
+  console.log('POST /')
+  console.dir(request.body)
+
+  let username = request.body.credentials.username
+  let gameID = request.body.gameID
+  let game = activeGames.find(game => game.gameID == gameID)
+
+  if (game == undefined) {
+    response.send("game not found")
+  } else if (game.gameState != 0) {
+    response.send("game already started")
+  } else if (!game.players.some(player => player.username == username)) {
+    response.send("you are not in this game")
+  }
+  else if (game.players.length < 2) {
+    response.send("not enough players")
+  }
+   else {
+    game.gameState = 1;
+    game.rounds = [[{
+      "player" : "", // Empty string, meaning that the server did something and not a player
+      "action" : 1  // Round started (the 1 would mean something different if it was a player)
+    }]]
+
+    response.send(game)
+  }
+})
 
 
 
