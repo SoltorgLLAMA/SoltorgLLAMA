8ada092f80c694628e0d05029027e8942643af53
Maillian99 <max.damberg@gmail.com>
2021-04-28 14:53:55 +0200

    Kortblandare

--- server/llamaSoltorg/routes/gameFunctions.js
+++ server/llamaSoltorg/routes/gameFunctions.js
@@ -1,6 +1,27 @@
 
 function shuffle() {
-    return "nåt fränt"
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+function getShuffledDeck() {
+    let deck = [1, 1, 1, 1, 1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2, 
+                3, 3, 3, 3, 3, 3, 3, 3, 
+                4, 4, 4, 4, 4, 4, 4, 4, 
+                5, 5, 5, 5, 5, 5, 5, 5, 
+                6, 6, 6, 6, 6, 6, 6, 6,
+                7, 7, 7, 7, 7, 7, 7, 7];
+    return shuffle(deck);
 }
 
 
